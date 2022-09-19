c477018c03cc37fd95753b4e12059b6ce4cf3f98
Gavin Falconer <gavin@expressivelogic.net>
2022-09-19 14:33:48 -0500

    Enable XML to be formatted if desired.

--- xprl-efactura-jaxb-publishing/src/main/kotlin/ec/com/xprl/efactura/jaxb/JaxbComprobantePublisher.kt
+++ xprl-efactura-jaxb-publishing/src/main/kotlin/ec/com/xprl/efactura/jaxb/JaxbComprobantePublisher.kt
@@ -3,13 +3,19 @@
 package ec.com.xprl.efactura.jaxb
 
 import ec.com.xprl.efactura.*
-import jakarta.xml.bind.JAXBContext
-import jakarta.xml.bind.Marshaller
-import java.io.StringWriter
 import jakarta.validation.ConstraintViolation
 import jakarta.validation.Validation
 import jakarta.validation.ValidatorFactory
+import jakarta.xml.bind.JAXBContext
+import jakarta.xml.bind.Marshaller
+import java.io.StringReader
+import java.io.StringWriter
 import javax.xml.stream.XMLOutputFactory
+import javax.xml.transform.OutputKeys
+import javax.xml.transform.Transformer
+import javax.xml.transform.TransformerFactory
+import javax.xml.transform.stream.StreamResult
+import javax.xml.transform.stream.StreamSource
 
 
 const val JAXB_UTF8_ENCODING = "UTF-8"
@@ -17,6 +23,7 @@ const val JAXB_UTF8_ENCODING = "UTF-8"
 class JaxbComprobantePublisher<T: ComprobanteElectronico>(
     val ambiente: Ambiente,
     val validationEnabled: Boolean = true,
+    val formattedXml: Boolean = false,
     val factory: JaxbComprobanteFactory = JaxbComprobanteFactoryImpl.default(ambiente)
 ) : ComprobantePublisher<T> {
 
@@ -27,7 +34,7 @@ class JaxbComprobantePublisher<T: ComprobanteElectronico>(
             validate(jaxbComprobanteClass, jaxbComprobante)
         }
 
-        val xml = genXml(jaxbComprobanteClass, jaxbComprobante)
+        val xml = genXml(jaxbComprobanteClass, jaxbComprobante, formattedXml)
         return PublishedComprobante(
             comprobante = comprobante,
             schemaVersion = version,
@@ -46,7 +53,7 @@ class JaxbComprobantePublisher<T: ComprobanteElectronico>(
         }
     }
 
-    private fun genXml(klass: Class<*>, jaxbComprobante: Any): String {
+    private fun genXml(klass: Class<*>, jaxbComprobante: Any, formattedXml: Boolean): CharSequence {
         val jaxbContext = JAXBContext.newInstance(klass)
         val jaxbMarshaller = jaxbContext.createMarshaller()
         val stringWriter = StringWriter()
@@ -57,12 +64,26 @@ class JaxbComprobantePublisher<T: ComprobanteElectronico>(
         jaxbMarshaller.setProperty(Marshaller.JAXB_ENCODING, JAXB_UTF8_ENCODING)
         jaxbMarshaller.marshal(jaxbComprobante, writer)
         writer.flush()
-        return stringWriter.buffer.toString()
+
+        return if (formattedXml) {
+            formatXml(stringWriter.buffer)
+        } else {
+            stringWriter.buffer
+        }
     }
 
     private companion object {
         val validatorFactory: ValidatorFactory by lazy {
             Validation.buildDefaultValidatorFactory()
+        }
+
+        private fun formatXml(xml: CharSequence): CharSequence {
+            val t: Transformer = TransformerFactory.newInstance().newTransformer()
+            t.setOutputProperty(OutputKeys.INDENT, "yes")
+            t.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4")
+            val out = StringWriter()
+            t.transform(StreamSource(StringReader(xml.toString())), StreamResult(out))
+            return out.toString()
         }
     }
 }
